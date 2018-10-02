b2580d98b8a1235d7de6c04070c9a4fd33b3631c
ShuttleDDD <33392616+ShuttleDDD@users.noreply.github.com>
2018-10-03 00:58:40 +0800

    Create Shuttle_c++.cpp

---
+++ Shuttle_c++.cpp
@@ -0,0 +1,42 @@
+#随手交了个作业
+#include<iostream>
+using namespace std;
+int getPower(int x,int y){
+    int ans=1;
+    if (y<0) return 0;
+    else if (y==0) return 1;
+
+    for(int i=0;i<y;i++){
+        ans=ans*x;
+    }
+    return ans;
+
+}
+double getPower(double x, int y){
+    double ans=1;
+    if (y<0) return 0.0;
+    else if (y==0) return 1;
+
+    while(y!=0){
+        y=y-1;
+        ans*=x;
+
+    }
+    return ans;
+
+
+
+}
+
+int main(){
+    int a,m=0;
+    double b=0.0;
+    int ansi=0;
+    double ansd=0.0;
+
+    cin>>a>>b>>m;
+    ansi=getPower(a,m);
+    ansd=getPower(b,m);
+    cout<<ansi<<" "<<ansd;
+
+}
